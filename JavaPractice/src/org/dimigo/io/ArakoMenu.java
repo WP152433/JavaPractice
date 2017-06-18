074d06bb3081060dce778cd1f880f5a098a7c3d3
WP152433 <crcmc2000@gmail.com>
2017-06-18 19:24:00 +0900

    실습과제 제출

---
+++ JavaPractice/src/org/dimigo/io/ArakoMenu.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package org.dimigo.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.Writer;
+
+/**
+ * <pre>
+ * org.dimigo.io
+ *   |_ ArakoMenu.java
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 6. 18.
+ * </pre>
+ *
+ * @author : Dav
+ * @version : 1.0
+ */
+public class ArakoMenu {
+
+	public static void main(String[] args) throws IOException {
+		
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			Writer writer = new BufferedWriter(new FileWriter("files/menu.txt"));
+			BufferedReader reader = new BufferedReader(new FileReader("files/menu.txt"))){
+			
+			String data;
+			
+			while((data = br.readLine()) != null){
+				writer.write(data+"\n");
+			}
+			
+			writer.flush();
+			
+			System.out.println("<< 메뉴 출력 >>");
+			while((data = reader.readLine()) != null){
+				System.out.println(data);
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}	
+	}
+
+}
